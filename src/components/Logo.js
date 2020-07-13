5c06a004103b0a692d24f9579e745660a5d1c4b0
otompo <abagnasalifusasco@gmail.com>
2020-07-13 11:58:42 +0100

    Create Logo.js

---
+++ src/components/Logo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../components/Logo.css'
+export default function Logo (){
+    return(
+        <div id="logo">
+       <div className="logo-p">
+           P
+       </div>
+       <span style={{color:'#FF0000',fontWeight:'bold',fontSize:'20px',marginLeft:'15px'}}>
+           PaSE Covid-19 Tracker For West African Countries from May 1st, 2020
+       </span>
+       <span>
+           
+       </span>
+        </div>
+    )
+}
