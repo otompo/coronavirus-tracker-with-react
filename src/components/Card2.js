40819eed00b75fe9a1ce6e37d62ce405c7e127a7
otompo <abagnasalifusasco@gmail.com>
2020-07-13 11:58:30 +0100

    Create Card2.js

---
+++ src/components/Card2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Card.css'
+export default function Card2(props){
+    return(
+        <div className="card2">
+            <span>{props.title}</span>
+        </div>
+    )
+}
